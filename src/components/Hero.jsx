11ee91755e33f034181a63dc4ae0d6b8b1711f80
fayssalrifland <fayssal.elbouhamedy19@gmail.com>
2024-12-24 11:10:09 +0100

    change social media and name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -78,7 +78,7 @@ const Hero = () => {
                     </motion.button>
 
                     <div className="flex gap-6 flex-row text-4xl md:text-6xl text-purple-400 z-20">
-                        <motion.a whileHover={{ scale: 1.2 }} href="https://www.instagram.com/madara_fayssal/">
+                        <motion.a whileHover={{ scale: 1.2 }} href="https://github.com/fayssalrifland">
                             <AiOutlineGithub/>
                         </motion.a>
 
@@ -86,7 +86,7 @@ const Hero = () => {
                             <AiOutlineLinkedin/>
                         </motion.a>
 
-                        <motion.a whileHover={{ scale: 1.2 }} href="#">
+                        <motion.a whileHover={{ scale: 1.2 }} href="https://www.instagram.com/madara_fayssal/">
                             <AiOutlineInstagram/>
                         </motion.a>
                     </div>
