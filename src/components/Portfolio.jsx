7c56b391131c3044d8eaa0ff36d390cbb2017c3a
Madara Fayssal <fayssal.elbouhamedy19@gmail.com>
2024-12-30 11:41:17 -0800

    ok

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import project1 from "../assets/project1.png";
 import project2 from "../assets/project2.png";
 import project4 from "../assets/project4.png";
